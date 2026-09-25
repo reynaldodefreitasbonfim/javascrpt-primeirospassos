ab1e490af801181af5867192468421dd3d63e002
reynaldodefreitasbonfim <reynaldodefreitasbonfimcastor@gmail.com>
2026-09-25 12:05:08 -0300

    comecei a programar

---
+++ desafio.js
@@ -0,0 +1,14 @@
+let numero = prompt("digite um numero")
+
+
+for (let i = 0; i < 30; i = i + 1) {
+
+    document.write("O contador vale:" + i + "<br>");
+
+if (numero/3)
+    document.write("escreve o numero")
+
+
+
+
+}
---
+++ repeticao.js
@@ -0,0 +1,29 @@
+/*
+let senha ="123456";
+let entradaDoUsuario =prompt("digite sua senha");
+var usuarioAcertouAsenha = entradaDoUsuario == senha
+
+while(usuarioAcertouAsenha == false) {
+    alert("senha incorreta,  tente novamente")
+    entradaDoUsuario =prompt(" digite sua senha");
+    usuarioAcertouaSenha = entradaDoUsuario == senha;
+//codigo a ser repetido
+// ate a condicao ser satisfeita 
+} document.write("acesso leberado");*/
+var nomeDousuario = prompt("digite seu nome completo");
+document.write("nomeDo Usuario tem " + nomeDousuario.length + " letras<br>")
+for (let i = 0; i  < nomeDousuario.length; i = i + 1) {
+    document.write("O contador vale:" +  i + "<br>" );
+ } // codigo a ser repetido
+// ate que a contador atinja a condicao de parada
+
+
+
+
+
+/*do {
+//codigo a ser execultado AO MENOS uma vez
+//ate a condiçao ser satisfeita
+}while(condicao);
+*/
+
